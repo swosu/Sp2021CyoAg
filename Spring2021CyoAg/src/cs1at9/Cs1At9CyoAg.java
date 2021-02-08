2391094b5b346110aadbe849ffca7e52739d0e52
evertj <evertj@EvertJ-PC3.admin.swosu.edu>
2021-02-08 09:29:15 -0600

    start of dev branch

--- Spring2021CyoAg/src/cs1at9/Cs1At9CyoAg.java
+++ Spring2021CyoAg/src/cs1at9/Cs1At9CyoAg.java
@@ -5,6 +5,11 @@ public class Cs1At9CyoAg {
 	public static void main(String[] args) {
 		System.out.println("Hello Class on an icy morning.");
 		System.out.println("I hope you all have some fun playing with this game.");
+		System.out.println("We are goign to get started.");
+		
+		
+		
+		int userSelect01 = 1;
 
 	}
 
