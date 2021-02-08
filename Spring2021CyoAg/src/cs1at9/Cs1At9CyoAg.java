eb63b87284656da50a5d9418303eae93fa77f215
Jeremy Evert <jeremy-evert@users.noreply.github.com>
2021-02-08 09:23:38 -0600

    Update Cs1At9CyoAg.java

--- Spring2021CyoAg/src/cs1at9/Cs1At9CyoAg.java
+++ Spring2021CyoAg/src/cs1at9/Cs1At9CyoAg.java
@@ -4,6 +4,7 @@ public class Cs1At9CyoAg {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Class on an icy morning.");
+		System.out.println("I hope you all have some fun playing with this game.");
 
 	}
 
