91b14b79d4776c6a6619acb9bdb36cb0e0ddb40c
evertj <evertj@EvertJ-PC3.admin.swosu.edu>
2021-02-08 10:39:16 -0600

    start of 10:00 cyoag.

--- Spring2021CyoAg/src/cs1at10/CyoAgCs1At10.java
+++ Spring2021CyoAg/src/cs1at10/CyoAgCs1At10.java
@@ -4,7 +4,7 @@ public class CyoAgCs1At10 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello.");
-System.out.println("please have some fun.");
+		System.out.println("please have some fun.");
 	}
 
 }
