48e2409b6b2621b653739a139c491e9f31131409
Jeremy Evert <jeremy-evert@users.noreply.github.com>
2021-02-08 06:27:51 -0600

    Update HelloCyoAg.java

--- Spring2021CyoAg/src/HelloCyoAg.java
+++ Spring2021CyoAg/src/HelloCyoAg.java
@@ -3,6 +3,7 @@ public class HelloCyoAg {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Choose Your Own Adventure Game.");
+		System.out.println("Please try to have some fun with this project.");
 
 	}
 
