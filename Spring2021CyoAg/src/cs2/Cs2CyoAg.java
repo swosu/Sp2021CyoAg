a8f728fd7cf9281156fb3907bcfa48f11316eb39
Jeremy Evert Helm <jeremy.evert@swosu.edu>
2021-02-10 06:53:29 -0600

    The start of a pop up window game.

--- Spring2021CyoAg/src/cs2/Cs2CyoAg.java
+++ Spring2021CyoAg/src/cs2/Cs2CyoAg.java
@@ -3,10 +3,29 @@ package cs2;
 public class Cs2CyoAg {
 
 	public static void main(String[] args) {
-		System.out.println("Hello...");
+		System.out.println("Hello, World.");
 		
-		MusicPlayer tune = new MusicPlayer();
-		tune.play();
+		GamePlay info = new GamePlay();
+		
+		info.greetPlayer();
+		
+		info.explainProcess();
+		int n = 0;
+		while(true) {
+			
+			n = info.keepPlaying();
+			
+			
+			if(1 == n) {
+				break;
+			}
+		}
+		
+		info.lastPopUp();
+		
+		//GameData card = new GameData();
+		
+		//card.getPlayerName();
 
 	}
 
